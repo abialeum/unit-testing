9ad92164285e1a34962aae2cfe3dbced922304a8
Abia <abia@alterra.id>
2022-10-31 00:08:09 +0700

    Example getAction

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
 import unittest  
 class TestingSum(unittest.TestCase):  
 
+    def getAction(self,action):
+        if action == 'hungry':
+            return 'go to eat'
+        elif action == 'sleepy':
+            return 'go to sleep'
+        
     def test_sum(self):  
         self.assertEqual(sum([1, 4, 5]), 10, "It should be 10")  
-
+        self.assertEqual(10-5, 5, "It should be 5")  
+    
+    def test_getAction(self):
+        self.assertEqual(self.getAction('hungry'),'go to eat', "It should be 'go to eat'")
+        self.assertEqual(self.getAction('sleepy'),'go to sleep', "It should be 'go to sleep'")
   
 if __name__ == '__main__':  
     unittest.main()
